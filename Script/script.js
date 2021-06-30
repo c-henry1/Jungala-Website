6e94d5caf5e4b860cac668978844b602ba9dcd88
c-henry1 <78730639+c-henry1@users.noreply.github.com>
2021-06-30 11:49:59 +0100

    Initial commit

---
+++ Script/script.js
@@ -0,0 +1,23 @@
+
+
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-elements-r');
+   
+    
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+    
+        //burg animation
+        burger.classList.toggle('toggle');
+    });
+
+    nav.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        burger.classList.toggle('toggle');
+    });
+}
+
+navSlide();
+
+
